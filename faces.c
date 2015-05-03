d292ac755e01d12105d1d880b0b1f5f9c3a7c8a6
Trammell Hudson <hudson@osresearch.net>
2015-05-03 19:21:11 -0400

    unify spacing

--- faces.c
+++ faces.c
@@ -63,8 +63,8 @@ main(void)
 	stl_3d_t * const stl = stl_3d_parse(STDIN_FILENO);
 	if (!stl)
 		return EXIT_FAILURE;
-	const double inset_distance = 6;
-	const double hole_radius = 3.0/2;
+	const double inset_distance = 5;
+	const double hole_radius = 1.5;
 
 	int * const face_used = calloc(sizeof(*face_used), stl->num_face);
 
